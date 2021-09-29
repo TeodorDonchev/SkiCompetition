73aa5fdc3a09855b3ebe97d4d9f352a704dce94e
TeodorDonchev <teodorgdo@gmail.com>
2021-09-29 12:27:50 +0300

    Add project files.

---
+++ SkiCompetition/Models/SkiCompetitionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkiCompetition.Models
+{
+    public class SkiCompetitionContext : DbContext
+    {
+        public SkiCompetitionContext(DbContextOptions<SkiCompetitionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Competitor> Competitors { get; set; }
+    }
+}
